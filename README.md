7fcdeea63e3c004d371fe345af6da9ae95b6d241
Chathan Driehuys <chathan@driehuys.com>
2022-08-20 16:09:27 -0400

    feat: Add basic git config.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dotfiles
+# Dotfiles
+
+My dotfiles.
+
+## Prerequisites
+
+Install `git`. Otherwise you can't clone the repository.
+
+## Installation
+
+Use `install.sh` to make all the appropriate symlinks.
+
+## Useful Information
+
+### Changing Shells
+
+To change your shell, use:
+
+```bash
+chsh
+```
+
+Log out and back in for the change to take effect.
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -euf
+
+# Get an absolute path from a relative one. Abstracted into a function in case
+# we need a separate implementation for different OSs later.
+abspath() {
+    realpath "$1"
+}
+
+mklink() {
+    local abs_source="$(abspath $1)"
+    local dest="${HOME}/$2"
+    ln -fs "${abs_source}" "${dest}"
+    echo "Linked '${abs_source}' to '${dest}'"
+}
+
+mklink ./git/.gitconfig .gitconfig
