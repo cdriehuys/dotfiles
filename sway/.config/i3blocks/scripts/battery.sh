6f971fa72e867abfcd3b4f5a3b933a941a18e917
Chathan Driehuys <chathan@driehuys.com>
2024-03-24 09:20:10 -0400

    fix: Battery script works with no remaining time
    If the battery is full, `acpi --battery` doesn't report any remaining
    charge time estimate.
    
    Fixes #1

--- sway/.config/i3blocks/scripts/battery.sh
+++ sway/.config/i3blocks/scripts/battery.sh
@@ -4,13 +4,20 @@ set -euf
 
 full_status="$(acpi --battery)"
 percent="$(echo "${full_status}" | grep -E --only-matching '[0-9]{1,3}%')"
-remaining="$(echo "${full_status}" | grep -E --only-matching '[0-9:]+ remaining')"
-
-time="$(echo "${remaining}" | cut -d' ' -f 1)"
+remaining="$(echo "${full_status}" | grep -E --only-matching '[0-9:]+ remaining' || true)"
 
 # Return full and short text so we can optionally customize the background later
 # which MUST be the third line of output.
-echo " ${percent} (${time}) "
+
+# Full text depends on if there is remaining charge/discharge time
+if [ ! -z "${remaining}" ]; then
+    time="$(echo "${remaining}" | cut -d' ' -f 1)"
+    echo " ${percent} (${time}) "
+else
+    echo " ${percent} "
+fi
+
+# Short text is just the battery percentage.
 echo " ${percent} "
 
 # Set urgent flag below 5% or use orange below 20%
